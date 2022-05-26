52e255591ae16703d82bbbf412a75eabb908252f
aditya-shirwatkar <aditya.s@students.vnit.ac.in>
2022-05-26 19:20:33 +0530

    backing up messed up website

---
+++ README.md
@@ -0,0 +1,8 @@
+# My Website
+
+[NieR Automata](https://g.co/kgs/F7BcLa) stylized version of my website
+
+### Credits
+
+- https://github.com/ItsArul/itsarul.github.io
+- https://github.com/metakirby5/yorha
---
+++ script/js/app.js
@@ -0,0 +1,183 @@
+"use strict";
+
+
+// Footer
+
+let footer = $(`
+`);
+
+//"Scroll to top" button
+let upArrow = $(`
+  <button id="btnScrollToTop" onclick="scrollToTop()"><i class="fas fa-2x fa-angle-up"></i></button>
+  <link rel="stylesheet" type="text/css" href="./css/style.css" />
+  })
+`);
+
+$(document).ready(function () {
+  // updating the color of the swiper bullets (initial update of color)
+  updateColorOfSwiperBullets(localStorage.getItem("lightMode"));
+
+  //function for the "Scroll To Top" button to detect the footer
+  $(window).scroll(function () {
+    //The button will be hidden until we scroll more than the window's height
+    if ($(window).scrollTop() < $(window).height()) {
+      $("#btnScrollToTop").css("visibility", "hidden");
+    } else {
+      $("#btnScrollToTop").css("visibility", "visible");
+      //The button will change it's color when it hits the footer
+      if (
+        $(window).scrollTop() + $(window).height() >
+        $(document).height() - 838
+      ) {
+        // 838 should be changed if footer's height is changed so that the button changes it's color exactly when it hits the footer (preferably 14 less than the computer height of the footer)
+        $("#btnScrollToTop").css("background-color", "#6a00bb");
+      } else {
+        $("#btnScrollToTop").css("background-color", "#6a00bb");
+      }
+    }
+  });
+});
+
+//function to scroll to top
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
+
+// Window Loads
+$(function () {
+  let bodyElement = $(`body`);
+  bodyElement.prepend(header);
+  bodyElement.append(footer);
+  bodyElement.append(upArrow);
+  $("#btnScrollToTop").css("visibility", "hidden");
+
+  //toggler hamburger functions
+  const menuBtn = document.querySelector(".navbar-toggler");
+  let menuOpen = false;
+  menuBtn.addEventListener("click", () => {
+    if (!menuOpen) {
+      menuBtn.classList.add("open");
+      menuOpen = true;
+    } else {
+      menuBtn.classList.remove("open");
+      menuOpen = false;
+    }
+  });
+});
+
+// function for toggling hamburger is-active class
+
+$(function () {
+  $("#js-hamburger").on("click", function () {
+    $(this).toggleClass("is-active");
+  });
+});
+
+// Navbar current page highlight
+
+let loader = document.querySelector(".loader-container");
+
+window.addEventListener("load", vanish);
+
+function vanish() {
+  loader.classList.add("disappear");
+}
+$(function () {
+  $("a.nav-link").each(function () {
+    if ($(this).prop("href") == window.location.href) {
+      $(this).addClass("current-link");
+    }
+  });
+});
+
+//function to remove underline on hover
+
+$(document).ready(function () {
+  $("a.nav-link").hover(
+    function () {
+      $(this).removeClass("current-link");
+    },
+    function () {
+      if ($(this).prop("href") == window.location.href) {
+        $(this).addClass("current-link");
+      }
+    }
+  );
+});
+
+//consistent dark mode for page change
+if (localStorage.getItem("lightMode") == "dark") {
+  var app = document.getElementsByTagName("HTML")[0];
+  app.setAttribute("light-mode", "dark");
+
+  //to add dark theme to nav bar after its been loaded
+  window.addEventListener("load", function () {
+    var nav = document.getElementById("navbar");
+    nav.classList.add("dark-theme");
+    document.getElementById("dark_toggler").checked = true;
+  });
+
+  var sc = document.getElementsByClassName("socialicon");
+  for (var i = 0; i < sc.length; i++) {
+    sc[i].classList.add("dsc");
+  }
+} else {
+  localStorage.setItem("lightMode", "light");
+}
+
+function toggle_light_mode() {
+  console.log(localStorage.getItem("lightMode"));
+  var app = document.getElementsByTagName("HTML")[0];
+  var nav = document.getElementById("navbar");
+  if (localStorage.lightMode == "dark") {
+    localStorage.lightMode = "light";
+    app.setAttribute("light-mode", "light");
+    nav.classList.remove("dark-theme");
+    var sc = document.getElementsByClassName("socialicon");
+    for (var i = 0; i < sc.length; i++) {
+      sc[i].classList.remove("dsc");
+    }
+  } else {
+    nav.classList.add("dark-theme");
+    localStorage.lightMode = "dark";
+    app.setAttribute("light-mode", "dark");
+    var sc = document.getElementsByClassName("socialicon");
+    for (var i = 0; i < sc.length; i++) {
+      sc[i].classList.add("dsc");
+    }
+  }
+
+  // updating the swiper bullets
+  updateColorOfSwiperBullets(localStorage.getItem("lightMode"));
+}
+
+// function to update swiper bullets
+function updateColorOfSwiperBullets(lightMode) {
+  document.querySelectorAll(".swiper-pagination-bullet").forEach((bullet) => {
+    if (lightMode == "light") {
+      bullet.style.backgroundColor = "blue";
+    } else {
+      bullet.style.backgroundColor = "white";
+    }
+  });
+}
+
+window.addEventListener("storage", function () {
+  if (localStorage.lightMode == "dark") {
+    app.setAttribute("light-mode", "dark");
+  } else {
+    app.setAttribute("light-mode", "light");
+  }
+});
+
+// Function to remove scroll bar during preload
+$(window).on("load", function () {
+  setTimeout(function () {
+    $(".no-scroll-preload").css("overflow", "visible");
+  }, 1000);
+  $(".loader-container").fadeOut(2500);
+});
